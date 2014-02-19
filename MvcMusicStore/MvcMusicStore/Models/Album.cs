de207ce38344ff4555eafca6a5199e399dfde5f9
Christopher Langsford <langz020@gmail.com>
2014-02-19 14:34:45 +0200

    Commit 3
    ch6. added data model annotations and validation rules

--- MvcMusicStore/MvcMusicStore/Models/Album.cs
+++ MvcMusicStore/MvcMusicStore/Models/Album.cs
@@ -6,6 +6,9 @@
 //    Manual changes to this file will be overwritten if the code is regenerated.
 // </auto-generated>
 //------------------------------------------------------------------------------
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace MvcMusicStore.Models
 {
@@ -20,11 +23,24 @@ namespace MvcMusicStore.Models
             this.OrderDetails = new HashSet<OrderDetail>();
         }
     
+        [ScaffoldColumn(false)]
         public int AlbumId { get; set; }
+
+        [DisplayName("Genre")]
         public int GenreId { get; set; }
+
+        [DisplayName("Artist")]
         public int ArtistId { get; set; }
+
+        [Required(ErrorMessage = "An Album Title is required")]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "Price is required")]
+        [Range(0.01, 100.00, ErrorMessage = "Price must be between 0.01 and 100")]
         public decimal Price { get; set; }
+
+        [DisplayName("Album Art Url")]
+        [StringLength(1024)]
         public string AlbumArtUrl { get; set; }
     
         public virtual Artist Artist { get; set; }
