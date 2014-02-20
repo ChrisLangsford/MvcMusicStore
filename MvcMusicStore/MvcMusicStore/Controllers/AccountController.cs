993a1e9e203a2c58621127ed8743b19358eac202
Christopher Langsford <langz020@gmail.com>
2014-02-20 17:21:52 +0200

    Commit 6
    Completed ch9, added checkout functionality

--- MvcMusicStore/MvcMusicStore/Controllers/AccountController.cs
+++ MvcMusicStore/MvcMusicStore/Controllers/AccountController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using System.Web.Routing;
 using System.Web.Security;
 using Mvc3ToolsUpdateWeb_Default.Models;
+using MvcMusicStore.Models;
 
 namespace Mvc3ToolsUpdateWeb_Default.Controllers
 {
@@ -30,6 +31,8 @@ namespace Mvc3ToolsUpdateWeb_Default.Controllers
             {
                 if (Membership.ValidateUser(model.UserName, model.Password))
                 {
+                    MigrateShoppingCart(model.UserName);
+
                     FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);
                     if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
                         && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
@@ -83,6 +86,7 @@ namespace Mvc3ToolsUpdateWeb_Default.Controllers
 
                 if (createStatus == MembershipCreateStatus.Success)
                 {
+                    MigrateShoppingCart(model.UserName);
                     FormsAuthentication.SetAuthCookie(model.UserName, false /* createPersistentCookie */);
                     return RedirectToAction("Index", "Home");
                 }
@@ -189,5 +193,14 @@ namespace Mvc3ToolsUpdateWeb_Default.Controllers
             }
         }
         #endregion
+
+        private void MigrateShoppingCart(string UserName)
+        { 
+            //Associate shopping cart items with logged-in user
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            cart.MigrateCart(UserName);
+            Session[ShoppingCart.CART_SESSION_KEY] = UserName;
+        }
     }
 }
--- MvcMusicStore/MvcMusicStore/Models/Order.cs
+++ MvcMusicStore/MvcMusicStore/Models/Order.cs
@@ -11,26 +11,65 @@ namespace MvcMusicStore.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Web.Mvc;
     
     public partial class Order
     {
+        [Bind(Exclude ="OrderId")]
         public Order()
         {
             this.OrderDetails = new HashSet<OrderDetail>();
         }
     
+        [ScaffoldColumn(false)]
         public int OrderId { get; set; }
+
+        [ScaffoldColumn(false)]
         public System.DateTime OrderDate { get; set; }
+
+        [ScaffoldColumn(false)]
         public string Username { get; set; }
+
+        [Required(ErrorMessage="First Name is required")]        
+        [StringLength(160)]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        [StringLength(160)]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        [StringLength(70)]
         public string Address { get; set; }
+
+        [Required(ErrorMessage = "City is required")]
+        [StringLength(40)]
         public string City { get; set; }
+
+        [Required(ErrorMessage = "State is required")]
+        [StringLength(40)]
         public string State { get; set; }
+
+        [Required(ErrorMessage = "Postal code is required")]
+        [StringLength(10)]
         public string PostalCode { get; set; }
+
+        [Required(ErrorMessage = "Country is required")]
+        [StringLength(40)]
         public string Country { get; set; }
+
+        [Required(ErrorMessage = "Phone is required")]
+        [StringLength(24)]
         public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Email address is required")]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}",
+                            ErrorMessage="Email is not valid.")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+        [ScaffoldColumn(false)]
         public decimal Total { get; set; }
     
         public virtual ICollection<OrderDetail> OrderDetails { get; set; }
